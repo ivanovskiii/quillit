da93cea67b52dfe8a41b641db388f087ac317169
Gorjan Ivanovski 183092 <gorjan.ivanovski@students.finki.ukim.mk>
2023-11-15 13:24:25 +0100

    Initial Commit

---
+++ Quillit/QuillitApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuillitApp.swift
+//  Quillit
+//
+//  Created by Gorjan Ivanovski on 15.11.23.
+//
+
+import SwiftUI
+
+@main
+struct QuillitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
