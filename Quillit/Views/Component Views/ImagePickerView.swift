eab7dcdeefd7fdac8057823b168b368c241b0898
Gorjan Ivanovski 183092 <gorjan.ivanovski@students.finki.ukim.mk>
2023-12-05 13:42:27 +0100

    Added Profile Pictures

---
+++ Quillit/Views/Component Views/ImagePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerView.swift
+//  Quillit
+//
+//  Created by Gorjan Ivanovski on 5.12.23.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerView()
+}
